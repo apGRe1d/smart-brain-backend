5b6a0f1763c59f3b8d93cc384367bb4e2473ef1a
apGRe1d <apGRe1d@yandex.ru>
2019-09-28 22:31:29 +0300

    first adding

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser'); 
+const bcrypt = require('bcryptjs');
+const cors = require('cors'); 
+const knex = require('knex');  
+
+const register = require('./controllers/register');
+const signIn = require('./controllers/signIn');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json()); 
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+//SIGN IN
+app.post('/signin', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) })
+
+//REGISTER
+app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcrypt) })
+
+//USER 
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+//IMAGE PART - updating the score of user
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+/*ENDPOINT FOR BACKEND CLARIFAI*/
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) })
+
+app.listen(3001, () => {
+	console.log('app is running on port 3001');
+})
+
+
