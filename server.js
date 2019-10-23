702716fc298c775c1eef5b778147ab7871f67b0f
apGRe1d <apGRe1d@yandex.ru>
2019-10-23 21:57:23 +0300

    urls

--- server.js
+++ server.js
@@ -18,9 +18,17 @@ const db = knex({
 });
 
 const app = express();
+const url = require('url');
 
 app.use(bodyParser.json()); 
 app.use(cors());
+
+
+app.use(express.static(url.parse('https://github.com/apGRe1d/face-recognition-production/tree/master/build', true)));
+    
+app.get('*', function(req, res) {
+	res.sendFile(url.parse('https://github.com/apGRe1d/face-recognition-production/tree/master/build/index.html', true));
+});
 
 
 app.get('/', (req, res) => {
@@ -37,7 +45,7 @@ app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcryp
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
 //IMAGE PART - updating the score of user
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) }) 
 
 /*ENDPOINT FOR BACKEND CLARIFAI*/
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) })
@@ -45,5 +53,11 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) })
 app.listen(process.env.PORT || 3001, () => {
 	console.log(`app is running on port ${process.env.PORT} 3001`);
 })
+
+
+
+
+
+
 
 
